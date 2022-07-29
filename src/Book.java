59f65a4eee0bdb06f2c82fe81df2e36ca6a9321a
Diana Bayazitova <onlydi@mail.ru>
2022-07-29 21:50:36 +0500

    Book.java Ura

--- src/Book.java
+++ src/Book.java
@@ -20,30 +20,17 @@ public class Book {
         System.out.println ("Nazvanie knigi "+ name+", "+" Nazvanie avtora "+author+ ", "+ " God vipuska "+ year+", "+"reiting " +3+", "+ "Tekst knigi "+":"+text);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ocenite knigu ot 0 do 5 ili viberete avtomaticheskuyu ocenku, napisav (20)");
+        System.out.println ("Ura!");
         raiting = scanner.nextInt();
         if (raiting <5){
             raiting++;}
         else {
             raiting--;
         }
-        //if (increase==20) {
-        //    increase=increase+1;}
-
-        //    if (increase > 5) {
-          //      increase=increase-1;
-
-         //   } else if (0 < increase && increase < 5) {
-            //    System.out.println("Vasha ocenka uchtena");
-          //  } else {
-           //     System.out.println("Reiting ne mozhet bit' men'she 0 i bol'she 5");
-          //  }
-        }
+                }
 
         public void append(String moreText){
-                   // Scanner scanner = new Scanner(System.in);
-            //System.out.println("Vvedite text knigi " + name);
-           // moreText = scanner.nextLine();
-            text += moreText;
+        text += moreText;
             System.out.println("Nazvanie knigi "+ name+", "+" Nazvanie avtora "+author+ ", "+ " God vipuska "+ year+", "+"reiting " +3+", "+ "Tekst knigi "+":"+text);
         }
     }
