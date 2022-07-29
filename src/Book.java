5b9ebfff55b34dfdf2d97a39467ab47c2962f29d
Diana Bayazitova <onlydi@mail.ru>
2022-07-29 22:05:00 +0500

    Book.java is changed

--- src/Book.java
+++ src/Book.java
@@ -18,6 +18,7 @@ public class Book {
 
     public void up(int raiting) {
         System.out.println ("Nazvanie knigi "+ name+", "+" Nazvanie avtora "+author+ ", "+ " God vipuska "+ year+", "+"reiting " +3+", "+ "Tekst knigi "+":"+text);
+        System.out.println ("Vot");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ocenite knigu ot 0 do 5 ili viberete avtomaticheskuyu ocenku, napisav (20)");
         raiting = scanner.nextInt();
@@ -29,9 +30,7 @@ public class Book {
               }
 
         public void append(String moreText){
-                   // Scanner scanner = new Scanner(System.in);
-            //System.out.println("Vvedite text knigi " + name);
-           // moreText = scanner.nextLine();
+
             text += moreText;
             System.out.println("Nazvanie knigi "+ name+", "+" Nazvanie avtora "+author+ ", "+ " God vipuska "+ year+", "+"reiting " +3+", "+ "Tekst knigi "+":"+text);
         }
